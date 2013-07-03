326588b6312bf9e7b883dc4c53e56abcea12ea98
T.M.Perry <tomperry7@gmail.com>
2013-07-03 08:02:58 -0500

    draw Z,W lines option for QCD/Data omission

--- CRAB/MuNu/scripts/parameters.py
+++ CRAB/MuNu/scripts/parameters.py
@@ -6,26 +6,48 @@ Author: T.M.Perry
 
 def arams():
  lumi = 13498. #luminosity 
- bNr = 2       # nr of b's
- btype = 't'   # tag t,m,l
- jNr = 2       # nr of jets
- I = 0         # print integral from 
- F = 20        # ... to in .log
+ bNr = 2
+ btype = 't'
+ jNr = 2
+ njetcut = '30' #20,25,26,30,40
+ jetcut = '30'
+ I = 0
+ F = 20
  iso_value = 0.12
  antiIso_value = 0.2
+ wSplitting = 'had'
+ #wSplitting = 'par'
 
  #drawQCD = False
  drawQCD = True
+ #drawData = False
+ drawData = True
  drawW = False
  #drawW = True
  drawZ = False
  #drawZ = True
 
  #naming where output goes
- path = '../plots/jets20_'
+ path = '../plots/exact'
  fitrange = '_'+str(I)+'_'+str(F)
- extraName = str(jNr)+'j'+str(bNr)+'b'+btype+fitrange
+ extraName = str(jNr)+'j'+njetcut+jetcut+'_'+str(bNr)+'b'+btype+'_'+wSplitting
+ #extraName = str(jNr)+'j'+str(bNr)+'b'+btype+fitrange
+ #extraName = str(jNr)+'j'+njetcut+jetcut+'_'+str(bNr)+'b'+btype+fitrange
 
- leafs = ['muonPt','MET','vertices','muonPhi','WPt','DiMuonMass','mJ3J4','ptJJ','met','MtCal','muonEta','ht','highestJetEta','secondJetEta','thirdJetEta','highestJetPhi','secondJetPhi','thirdJetPhi','highestJetPt','secondJetPt','thirdJetPt']
+ #leafs = ['highestJetPt','highestJetEta','highestJetPhi','secondJetPt','secondJetEta','secondJetPhi']
+ #leafs =['J1DR','J2DR']
+ #leafs=['muonPt','muonEta','muonPhi']
+ #leafs = ['DiMuonMass']
 
- return lumi,bNr,btype,jNr,I,F,iso_value,antiIso_value,path,extraName,leafs,drawW,drawZ,drawQCD
+ #leafs=['J1CSVbtag','J2CSVbtag']
+ #leafs=['J1JetParton','J2JetParton']
+ #leafs = ['LHEProduct']
+
+ #leafs=['Mt','WPt','ptJJ','ht']
+
+ #leafs = ['mJ3J4','thirdJetEta','thirdJetPhi','thirdJetPt']
+
+ #leafs = ['MET','vertices']
+
+ leafs = ['highestJetPt','highestJetEta','highestJetPhi','secondJetPt','secondJetEta','secondJetPhi','J1DR','J2DR','muonPt','muonEta','muonPhi','Mt','WPt','ptJJ','ht','MET','vertices']
+ return lumi,bNr,btype,jNr,njetcut,jetcut,I,F,iso_value,antiIso_value,path,extraName,leafs,drawW,drawZ,drawQCD,drawData,wSplitting
