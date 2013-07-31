61d3cfb2efedc683e15c0bc64a1efa3ca8ee13ee
T.M.Perry <tomperry7@gmail.com>
2013-07-31 15:41:30 +0200

    add CMSPrelim without lumi

--- CRAB/MuNu/scripts/cmsPrelim.py
+++ CRAB/MuNu/scripts/cmsPrelim.py
@@ -19,3 +19,8 @@ def prelim_alt(lumi):
  tex.SetTextAlign(33)
  lumiTitle = '#int L dt = %.1f fb^{-1}' %(lumi/1000.)
  tex.DrawLatex(0.87,0.87,lumiTitle)
+
+def prelim_noLumi():
+ tex.SetTextSize(0.07)
+ tex.SetTextAlign(13) #left, top 
+ tex.DrawLatex(0.11,0.89,'#font[32]{CMS Preliminary}')
