bbf3a755c4f792e1f91b91fc2bc7c8912a6a2617
T.M.Perry <tomperry7@gmail.com>
2013-08-08 13:47:54 +0200

    add paths for single top, ttbar, signal, control, ...

--- CRAB/MuNu/scripts/parameters.py
+++ CRAB/MuNu/scripts/parameters.py
@@ -6,27 +6,54 @@ Author: T.M.Perry
 
 def arams():
  lumi = 19429.
- bNr = 2
  btype = 't'
- jNr = 2
  njetcut = '25' #20,25,26,30,40
  jetcut = '25'
  I = 0
  F = 20
  iso_value = 0.12
  antiIso_value = 0.2
- wSplitting = 'had'
- #wSplitting = 'par'
 
  drawQCD = True
  drawData = True
 
- jetVeto = True #use == instead of >=
-
- Control = True #one muon
- noMT = False # no mT cut
+ Control = False #one muon
+ Signal = False
+ noMT = False # control but no mT cut
+ TT_m = False #1 mu, 2b's, 4 j
+ TT_me = False #no b's, 1mu 1e
+ ST = True
  Z_Region = False # two muons, no mT cut
- legacy = False #voids everything else and puts parametrs from 7Tev analysis
+ Legacy = False #voids everything else and puts parametrs from 7Tev analysis
+ 
+ bNr = 0
+ jNr = 2
+ jetVeto = False
+
+ if Signal or Legacy:
+  bNr = 2
+  jNr = 2
+  jetVeto = True
+ elif Control or noMT:
+  bNr = 0 
+  jNr = 2
+  jetVeto = False
+ elif TT_m:
+  bNr = 2
+  jNr = 4
+  jetVeto = True
+ elif TT_me:
+  bNr = 0 
+  jNr = 4
+  jetVeto = False
+ elif Z_Region:
+  bNr = 0
+  jNr = 2
+  jetVeto = False
+ elif ST:
+  bNr = 1
+  jNr = 2
+  jetVeto = False
 
  drawW = False # line at W when plotting
  drawZ = False # line at Z when plotting
@@ -34,17 +61,17 @@ def arams():
  #naming where output goes
  path = '../plots/'
  fitrange = '_'+str(I)+'_'+str(F)
- extraName = str(jNr)+'j'+njetcut+jetcut+'_'+str(bNr)+'b'+btype+'_newBTAG'
+ extraName = str(jNr)+'j'+njetcut+jetcut+'_'+str(bNr)+'b'+btype+'_oldT'
 
  jet1_2dr2 = '(((highestJetEta-secondJetEta)*(highestJetEta-secondJetEta))+((highestJetPhi-secondJetPhi)*(highestJetPhi-secondJetPhi)))'
 
- leafs = ['highestJetPt','highestJetEta','highestJetPhi','secondJetPt','secondJetEta','secondJetPhi']
+ #leafs = ['highestJetPt','highestJetEta','highestJetPhi','secondJetPt','secondJetEta','secondJetPhi','MET']
  #leafs = ['secondJetPt','secondJetEta','secondJetPhi']
  #leafs = ['highestJetPt']
- #leafs = ['highestJetPt','highestJetEta','highestJetPhi']
+ leafs = ['highestJetPt','highestJetEta','highestJetPhi','muonCharge']
  #leafs =['J1DR','J2DR']
  #leafs=['muonPt']
- #leafs=['nJetsPt20']
+ #leafs=['nJetsPt30']
  #leafs=['muonPt','muonEta','muonPhi']
  #leafs = ['DiMuonMass']
  
@@ -58,7 +85,7 @@ def arams():
  #leafs=['J1JetParton','J2JetParton']
  #leafs = ['LHEProduct']
 
- #leafs=['Mt','WPt','ptJJ','ht']
+ #leafs=['MET','WPt','ptJJ','ht']
  #leafs=['Mt']
 
  #leafs = ['mJ3J4','thirdJetEta','thirdJetPhi','thirdJetPt']
@@ -66,7 +93,9 @@ def arams():
  #leafs = ['MET','vertices']
  #leafs = ['vertices']
 
- #leafs = ['highestJetPt','highestJetEta','highestJetPhi','secondJetPt','secondJetEta','secondJetPhi','J1DR','J2DR','muonPt','muonEta','muonPhi','Mt']#,'WPt','ptJJ','ht','MET','vertices']
- #leafs = ['secondJetPt','secondJetEta','secondJetPhi','J1DR','J2DR','muonPt','Mt','WPt','ptJJ','ht','MET','vertices']
+ #leafs = ['secondJetEta','secondJetPhi','J1DR','J2DR','muonPt','muonEta','muonPhi','Mt','WPt','ptJJ','ht','MET','vertices']
+ #leafs = ['highestJetPt','highestJetEta','highestJetPhi','secondJetPt','secondJetEta','secondJetPhi','J1DR','J2DR','muonPt','muonEta','muonPhi','Mt','WPt','ptJJ','ht','MET','vertices']
+ #leafs = ['J1DR','J2DR','muonPt','muonEta','muonPhi','Mt','WPt','ptJJ','ht','MET','vertices',jet1_2dr2,'nJetsPt30','nJetsPt20']
 
- return lumi,bNr,btype,jNr,njetcut,jetcut,I,F,iso_value,antiIso_value,path,extraName,leafs,drawW,drawZ,drawQCD,drawData,wSplitting,jetVeto,Control,Z_Region,legacy,noMT
+ return lumi,bNr,btype,jNr,njetcut,jetcut,I,F,iso_value,antiIso_value,path,extraName,leafs,drawW,drawZ,drawQCD,drawData,jetVeto,Control,Z_Region,Legacy,noMT,TT_m,TT_me,ST,Signal
+
