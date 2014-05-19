f5f1ab8b52c2ae9ed3e96913973a7bb70eff3597
T.M.Perry <tomperry7@gmail.com>
2014-05-19 13:50:45 +0200

    for synching purposes

---
+++ CRAB/MuNu/scripts/SimpleCompare.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+'''
+Compares the shapes of variables from two data.root files
+Author: T.M.Perry UW
+'''
+import ROOT
+from ROOT import THStack,TH1F,TFile
+from ROOT import TLegend,TCanvas,TPad,TLatex,TLine
+from ROOT import gROOT,gStyle
+
+import aHisto as h #function to make histograms
+import histoRange as hr #manages range, lables for plots
+
+path = '../scripts/synch/Data_norm_comp_wbb'
+#out_file = gROOT.FindObject("%s.root"%(path))
+#if out_file: out_file.Close()
+#out_file = TFile("%s.root"%(path),"RECREATE","synching")
+
+tom_filename = '../scripts/synch/Data_norm_wbb.root' 
+and_filename = '../scripts/synch/Data_norm_Andrea_SingleMuonB_v09_dumphistos.root'
+tom_file = TFile(tom_filename)
+and_file = TFile(and_filename)
+
+tom_leafs = [
+'J1_pt',
+'J2_pt',
+'muon_pt',
+'met_pt',
+'mt'
+]
+and_leafs = [
+'h_first_bjet_pt',
+'h_second_bjet_pt',
+'h_first_muon_pt_bb',
+'h_MET_bb',
+'h_mt_wmnu_bb_wide'
+]
+
+jel_filenames = [
+'../scripts/synch/Data_norm_Jelena_maxhJet_pt0hJet_ptSingleMuBJetNormalized.root',
+'../scripts/synch/Data_norm_Jelena_minhJet_pt0hJet_ptSingleMuBJetNormalized.root',
+'../scripts/synch/Data_norm_Jelena_vLepton_pt0SingleMuBOtherNormalized.root',
+'../scripts/synch/Data_norm_Jelena_METtype1corretSingleMuBOtherNormalized.root',
+'../scripts/synch/Data_norm_Jelena_VMtSingleMuBOtherNormalized.root'
+]
+
+I = -1
+F = -1
+can = TCanvas('can','can',800,800)
+
+for leaf,a_leaf,jel_filename in zip(tom_leafs,and_leafs,jel_filenames):
+ jel_file = TFile(jel_filename)
+ steps, xmin, xmax, xtitle, xunits, setLogY = hr.ranger(leaf)
+ bmin = 1
+ bmax = steps+1
+
+ tomh = tom_file.Get(leaf)
+ tomh.SetName('tomh')
+ tomh.Scale(1./tomh.Integral())
+ tomh.SetLineColor(ROOT.EColor.kGreen+3)
+ tomh.SetLineWidth(3)
+ tomh.Draw()
+ tmax = tomh.GetMaximum()
+
+ jelh = jel_file.Get('data_obs')
+ jelh.SetName('jelh')
+ jelh.Scale(1./jelh.Integral())
+ jelh.SetLineColor(ROOT.EColor.kRed)
+ jelh.SetLineWidth(2)
+ jelh.Draw()
+ jmax = jelh.GetMaximum()
+
+ andh = and_file.Get(a_leaf)
+ andh.SetName('andh')
+ andh.Scale(1./andh.Integral())
+ andh.SetLineColor(ROOT.EColor.kBlue)
+ andh.SetLineWidth(1)
+ andh.Draw()
+ amax = andh.GetMaximum()
+
+ leg=TLegend(0.5,0.5,0.9,0.7)
+ leg.SetFillColor(0)
+ leg.SetBorderSize(0)
+ leg.AddEntry(tomh,'Tom W+bb','l')
+ leg.AddEntry(jelh,'Jelena W+bb','l')
+ leg.AddEntry(andh,'Andrea W+bb','l')
+
+ can.cd()
+ tomh.SetTitle(leaf)
+ tomh.SetMaximum(1.1*max(tmax,jmax,amax))
+ tomh.Draw('hist')
+ jelh.Draw('hist,sames')
+ andh.Draw('hist,sames')
+ leg.Draw('sames')
+ gStyle.SetOptStat('')
+
+ can.Update()
+ print('you just read '+leaf)
+# save = raw_input ('Press Enter to Continue (type save to save)\n')
+# if save == 'save':
+ can.Print(path+'_'+leaf+'.png') 
+ tomh.SetName('tom_'+leaf)
+ jelh.SetName('jelena_'+leaf)
+ andh.SetName('andrea_'+leaf)
+ #out_file.Write()
+ tomh.Delete()
+ jelh.Delete()
+ andh.Delete()
--- CRAB/MuNu/scripts/SimplePlotter.py
+++ CRAB/MuNu/scripts/SimplePlotter.py
@@ -13,9 +13,16 @@ import histoRange as hr #manages range, lables for plots
 
 data_filename = '../data/post_synch_v0/Data_B.root' 
 
+cut_type = 'wbb'
+path = '../scripts/synch/Data_norm_%s'%(cut_type)
+
 data_file  = TFile( data_filename)
 eventTreeLocation = 'muNuEventTree/eventTree'
 data_tree  =  data_file.Get(eventTreeLocation) 
+
+out_file = gROOT.FindObject("%s.root"%(path))
+if out_file: out_file.Close()
+out_file = TFile("%s.root"%(path),"RECREATE","synching")
 
 leafs = [
 'J1_pt',
@@ -25,14 +32,19 @@ leafs = [
 'mt'
 ]
 
-#cut = '(2>1)'
- #W+bb
-cut='(((J1_idLooseJet)&&(J2_idLooseJet))&&(muNuRelPFIsoDB_A<0.12)&&(((((HLT_IsoMu24_eta2p1_v_fired)&&(DiMuonMass<=60 && nrEle==0 && nrMu==1 && abs(muon_eta)<2.1 && muon_pt>30)&&(abs(dz)<0.5&&abs(ipDXY)<0.02)&&(2>1)&&(nJets24Pt25==0))&&(2>1))&&((J1_CSVbtag>0.898)&&(J2_CSVbtag>0.898)))&&((J1_pt >25 && J2_pt>25 && abs(J1_eta)<2.4 && abs(J2_eta)<2.4)&& J3_pt<25)) && mt<45)'
- #tt_e
+cut = '(2>1)'
+
+if cut_type is 'wbb':
+ cut='(((J1_idLooseJet)&&(J2_idLooseJet))&&(muNuRelPFIsoDB_A<0.12)&&(((((HLT_IsoMu24_eta2p1_v_fired)&&(nrEle==0 && nrMu==1 && abs(muon_eta)<2.1 && muon_pt>30)&&(nJets24Pt25==0))&&(2>1))&&((J1_CSVbtag>0.898)&&(J2_CSVbtag>0.898)))&&((J1_pt >25 && J2_pt>25 && abs(J1_eta)<2.4 && abs(J2_eta)<2.4)&& J3_pt<25)) && mt>45)'
+
+if cut_type is 'tt_me':
+ cut='(((J1_idLooseJet)&&(J2_idLooseJet))&&(muNuRelPFIsoDB_A<0.12)&&(((((HLT_IsoMu24_eta2p1_v_fired)&&(nrEle==1 && nrMu==1 && abs(muon_eta)<2.1 && muon_pt>30)&&(2>1)&&(mt>45))&&(2>1))&&(((J1_CSVbtag>0.898))))&&((J1_pt>25 && J2_pt>25 && abs(J1_eta)<2.4 && abs(J2_eta)<2.4) && J3_pt<25)) && mt>45)'
+
+if cut_type is 'tt_m':
+ cut='((((J1_idLooseJet)&&(J2_idLooseJet))&&(J3_idLooseJet))&&(muNuRelPFIsoDB_A<0.12)&&(((((HLT_IsoMu24_eta2p1_v_fired)&&(nrEle==0 && nrMu==1 && abs(muon_eta)<2.1 && muon_pt>30)&&(2>1)&&(mt>45))&&(2>1))&&((J1_CSVbtag>0.898)&&(J2_CSVbtag>0.898)))&&(((J1_pt >25 && J2_pt>25 && abs(J1_eta)<2.4 && abs(J2_eta)<2.4)&& J3_pt  > 25 && abs(J3_eta) <  2.4))))'
 
 I = -1
 F = -1
-path = '../plots/Data_norm_wbb'
 can = TCanvas('can','can',800,800)
 for leaf in leafs:
  steps, xmin, xmax, xtitle, xunits, setLogY = hr.ranger(leaf)
@@ -44,23 +56,23 @@ for leaf in leafs:
  hnwr.SetLineColor(ROOT.EColor.kGreen+3)
  hnwr.SetLineWidth(3)
 
- leg=TLegend(0.5,0.5,0.9,0.8)
+ leg=TLegend(0.5,0.5,0.9,0.7)
  leg.SetFillColor(0)
  leg.SetBorderSize(0)
  leg.AddEntry(hnwr,'Tom 2012B')
- #leg.AddEntry(hold,'Old Data')
- #leg.AddEntry(hnew,'New Data')
- #leg.AddEntry(hold,'Old Algo')
- #leg.AddEntry(hnew,'New Algo')
 
  can.cd()
  #hnwr.SetMaximum(0.07)
  hnwr.SetTitle(leaf)
  hnwr.Draw('hist')
  leg.Draw('sames')
+ gStyle.SetOptStat(1122)
 
  can.Update()
  print('you just read '+leaf)
 # save = raw_input ('Press Enter to Continue (type save to save)\n')
 # if save == 'save':
- can.Print(path+leaf+'.png') 
+ can.Print(path+'_'+leaf+'.png') 
+ hnwr.SetName(leaf)
+ out_file.Write()
+ hnwr.Delete()
